ab2efb0f7b179da65a44c13f90684786a049489f
felipesalvare <felipe.salvare@yahoo.com>
2023-08-31 11:57:46 -0300

    empieza_el_ej_10

---
+++ ejercicio10.R
@@ -0,0 +1,7 @@
+
+# EJERCICIO 10 ------------------------------------------------------------
+
+nmbre <- readline("Ingrese su nombre: ")
+aplldo <- readline("Ingrese su apellido: ")
+lbrt <- readline("Ingrese su libreta universitaria en formato N/AA: ")
+
